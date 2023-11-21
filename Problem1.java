6f424bafb0a9eba35c20d23f2a79d64a477c33c4
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-11-21 10:21:38 +0530

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+      
+        int numFlips = scanner.nextInt();
+
+ 
+        if (numFlips <= 0) {
+            System.out.println("Please enter a positive integer for the number of flips.");
+            return;
+        }
+
+        int headsCount = 0;
+        int tailsCount = 0;
+
+       
+        for (int i = 0; i < numFlips; i++) {
+         
+            if (Math.random() < 0.5) {
+                tailsCount++;
+            } else {
+                headsCount++;
+            }
+        }
+
+       
+        double percentageHeads = (headsCount * 100.0) / numFlips;
+        double percentageTails = (tailsCount * 100.0) / numFlips;
+
+        System.out.println(percentageHeads);
+        System.out.println(percentageTails);
+    }
+}
---
+++ Problem6.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Problem6 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        int dividend = scanner.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = scanner.nextInt();
+
+        
+        if (divisor == 0) {
+            System.out.println("Cannot divide by zero. Please enter a non-zero divisor.");
+            return;
+        }
+
+       
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+     
+        System.out.println("Quotient: " + quotient);
+        System.out.println();
+        System.out.println("Remainder: " + remainder);
+    }
+}
